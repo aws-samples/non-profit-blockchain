7664cf10eef9443085f5eb9d2306e97c74946973
Michael Edge <edgema@amazon.com>
2018-11-15 16:23:34 +0800

    initial commit

---
+++ ngo-rest-api/connection-profile/gen-connection-profile-tg.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+#REPODIR points to this repo.
+REPODIR=~/non-profit-blockchain
+
+#copy the connection profiles
+mkdir -p $REPODIR/tmp/connection-profile/org1
+mkdir -p $REPODIR/tmp/connection-profile/org2
+cp ngo-connection-profile-tg.yaml $REPODIR/tmp/connection-profile
+cp client-org1.yaml $REPODIR/tmp/connection-profile/org1
+cp client-org2.yaml $REPODIR/tmp/connection-profile/org2
+
+
+ls -lR $REPODIR/tmp/connection-profile
