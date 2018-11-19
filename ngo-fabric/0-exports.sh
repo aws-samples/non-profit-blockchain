dfe72d4c143a22cfb1932dcd6902f3b05114cc2e
Michael Edge <edgema@amazon.com>
2018-11-19 20:55:39 +0800

    initial commit

--- ngo-fabric/0-exports.sh
+++ ngo-fabric/0-exports.sh
@@ -15,6 +15,11 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+echo Downloading and installing model file for new service
+cd ~
+aws s3 cp s3://taiga-beta-test/service-2.json .  
+aws configure add-model --service-model file://service-2.json --service-name managedblockchain
+
 # Update these values to create a new Fabric network
 export REGION=us-east-1
 export ENDPOINT=https://taiga-beta.us-east-1.amazonaws.com
