21d1f97e34eb3e13162ffe18c5a42d3d3dd8ad0c
Michael Edge <edgema@amazon.com>
2018-11-19 11:13:59 +0800

    initial commit

--- ngo-fabric/2-vpc-client-node.sh
+++ ngo-fabric/2-vpc-client-node.sh
@@ -17,13 +17,28 @@
 
 echo Creating VPC - TODO. Create the VPC, subnets, security group, EC2 client node, VPC endpoint
 echo Create a keypair
+
+keyname=$(aws ec2 describe-key-pairs --key-names $NETWORKNAME-keypair --region $REGION --query 'KeyPairs[0].KeyName' --output text)
+echo Searching for existing keypair. Found $keyname
+if  [[ "$keyname" == "$NETWORKNAME-keypair" ]]; then
+    echo Keypair $NETWORKNAME-keypair already exists. Please choose another keypair name by editing this script
+    exit 1
+fi
+ 
 aws ec2 create-key-pair --key-name $NETWORKNAME-keypair --region $REGION --query 'KeyMaterial' --output text > ~/$NETWORKNAME-keypair.pem
+if [ $? -neq 0 ]
+    echo Keypair $NETWORKNAME-keypair could not be created
+    exit $?
+fi
+
 chmod 400 $NETWORKNAME-keypair.pem
 sleep 10
 
 echo Create the VPC, the Fabric client node and the VPC endpoints
 aws cloudformation deploy --stack-name fabric-client-node --template-file fabric-client-node.yaml \
 --capabilities CAPABILITY_NAMED_IAM \
---parameter-overrides KeyName=$NETWORKNAME-keypair \
+--parameter-overrides KeyName=$NETWORKNAME-keypair BlockchainVpcEndpointServiceName=$VPCENDPOINTSERVICENAME \
 --region $REGION
+
+aws cloudformation wait stack-create-complete --stack-name fabric-client-node
 
