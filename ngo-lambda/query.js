8f3cee31b89a63ce329bee8ee896341da4498bc0
Emile Baizel <baizele@amazon.com>
2019-09-26 12:57:45 -0700

    update error handling

--- ngo-lambda/query.js
+++ ngo-lambda/query.js
@@ -28,20 +28,23 @@ async function queryChaincode(request) {
     .then((query_responses) => {
         logger.info("Query has completed, checking results");
         // query_responses could have more than one  results if there multiple peers were used as targets
+        let result;
         if (query_responses && query_responses.length == 1) {
             if (query_responses[0] instanceof Error) {
-                logger.error("error from query = ", query_responses[0]);
+                throw new Error(query_responses[0]);
             } else {
-                logger.info("query responses is " + query_responses);
+                logger.info("query responses are " + query_responses);
                 logger.info("Response is ", query_responses[0].toString());
-                return query_responses[0].toString();
+                result = query_responses[0].toString();
             }
         } else {
             logger.info("No payloads were returned from query");
         }
         logger.info("=== Query Function End ===");
+        return result;
     }).catch((err) => {
-	    logger.error('Failed to query successfully :: ' + err);
+        logger.error('Failed to query successfully :: ' + err);
+        throw err;
     });
 }
 
