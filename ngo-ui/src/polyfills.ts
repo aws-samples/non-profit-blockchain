c3f62c7eb5995214f6ed4058134e165972e51a59
Michael Edge <edgema@amazon.com>
2020-04-27 11:37:58 +0800

    add reflect to resolve ui issue

--- ngo-ui/src/polyfills.ts
+++ ngo-ui/src/polyfills.ts
@@ -18,6 +18,9 @@
  * BROWSER POLYFILLS
  */
 
+import 'reflect-metadata';
+import 'core-js/es7/reflect';
+
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
 // import 'core-js/es6/symbol';
 // import 'core-js/es6/object';
